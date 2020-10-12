b7662beac0e5b794fa229952caa0575fc834b518
Bart <me@bartzalewski.com>
2020-10-13 00:47:07 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import useSwr from "swr";
+import GoogleMapReact from "google-map-react";
+import useSuperCluster from "use-supercluster";
 
-function App() {
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const Marker = ({ children }) => children;
+
+export default function App() {
+  // 1. map setup
+  const mapRef = useRef();
+  const [zoom, setZoom] = useState(10);
+  const [bounds, setBounds] = useState(null);
+
+  // 2. load and format data
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, fetcher);
+  const crimes = data && !error ? data : [];
+  const points = crimes.map((crime) => ({
+    type: "Feature",
+    properties: {
+      cluster: false,
+      crimeId: crime.id,
+      category: crime.category,
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(crime.location.longitude),
+        parseFloat(crime.location.latitude),
+      ],
+    },
+  }));
+
+  // 3. get clusters
+  const { clusters, supercluster } = useSuperCluster({
+    points,
+    bounds,
+    zoom,
+    options: { radius: 75, maxZoom: 20 },
+  });
+
+  // 4. render map
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY }}
+        defaultCenter={{ lat: 52.6376, lng: -1.135171 }}
+        defaultZoom={10}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map }) => {
+          mapRef.current = map;
+        }}
+        onChange={({ zoom, bounds }) => {
+          setZoom(zoom);
+          setBounds([
+            bounds.nw.lng,
+            bounds.se.lat,
+            bounds.se.lng,
+            bounds.nw.lat,
+          ]);
+        }}
+      >
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount,
+          } = cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker key={cluster.id} lat={latitude} lng={longitude}>
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 40}px`,
+                    height: `${10 + (pointCount / points.length) * 40}px`,
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      20
+                    );
+                    mapRef.current.setZoom(expansionZoom);
+                    mapRef.current.panTo({ lat: latitude, lng: longitude });
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+          return (
+            <Marker
+              key={cluster.properties.crimeId}
+              lat={latitude}
+              lng={longitude}
+            >
+              <button className="crime-marker">
+                <img src="/custody.svg" alt="crime doesn't pay" />
+              </button>
+            </Marker>
+          );
+        })}
+      </GoogleMapReact>
     </div>
   );
 }
-
-export default App;
